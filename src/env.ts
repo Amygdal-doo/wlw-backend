c914cb385ef6a629ae002de669fc39c6930fdfe6
One-Zee <harunadindane15@gmail.com>
2024-10-18 11:23:39 +0200

    added build command

--- src/env.ts
+++ src/env.ts
@@ -9,8 +9,26 @@ export const EnvSchema = z.object({
     PORT: z.coerce.number().default(3000),
     LOG_LEVEL: z.enum(["fatal" , "error" , "warn" , "info" , "debug" , "trace"]),
     DB_URL: z.string().url('Invalid database url'),
-
+    // OPTIONAL_ENV_IN_DEVELOPMENT: z.string().optional()
 })
+// .refine((input) => {
+//     if (input.NODE_ENV === 'production') {
+//         return !!input.OPTIONAL_ENV_IN_DEVELOPMENT
+//     }
+//     return true
+// })
+// .superRefine((input,ctx) => {
+//     if (input.NODE_ENV === 'production' && !input.OPTIONAL_ENV_IN_DEVELOPMENT) {
+//         ctx.addIssue({
+//             code: z.ZodIssueCode.invalid_type,
+//             expected: 'string',
+//             received: 'undefined',
+//             path: ['OPTIONAL_ENV_IN_DEVELOPMENT'],
+//             message: 'If NODE_ENV is production, OPTIONAL_ENV_IN_DEVELOPMENT must be set'
+//         })
+//         return !!input.OPTIONAL_ENV_IN_DEVELOPMENT
+//     }
+// })
 
 export type env = z.infer<typeof EnvSchema>
 
